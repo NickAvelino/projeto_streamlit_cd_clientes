2a09e5fe8be0b407212c87328845482d9b3a2332
Robenildo Avelino <136732701+NickAvelino@users.noreply.github.com>
2024-09-30 12:28:46 -0300

    Arquivos enviados

---
+++ Cadastros.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+from datetime import date
+
+def gravar_dados(nome, dt_nasc, tipo):
+    if nome and dt_nasc <= date.today():
+        with open("clientes.csv", "a", encoding="utf-8") as file:
+            file.write(f"{nome},{dt_nasc},{tipo}\n")
+        st.session_state['sucesso'] = True
+    else:
+        st.session_state['sucesso'] = False
+
+st.set_page_config(
+    page_title="Cadastro de clientes",
+    page_icon="👋",
+)
+
+st.title("Cadastro de clientes")
+st.divider()
+
+nome = st.text_input("Digite o nome do cliente",
+                     key="nome_cliente",)
+
+dt_nasc = st.date_input("Data de nascimento", format="DD/MM/YYYY", key="nasc")
+
+tipo = st.selectbox("Tipo de Cliente", ["Pessoa Jurídica", "Pessoa Física"] )
+
+btn_cadastrar = st.button("Cadastrar",
+                          on_click=gravar_dados,
+                          args=[nome, dt_nasc, tipo])
+
+if btn_cadastrar:
+    if st.session_state['sucesso']:
+        st.success("Cliente Cadastrado com sucesso", icon="✅")
+    else:
+        st.error("Erro ao cadastrar o cliente", icon="🚨")
